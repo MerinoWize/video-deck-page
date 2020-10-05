2b035be05491a1c1ea82cf4475b522ce10cf908d
Eduardo Merino <eduardo.merino@dowjones.com>
2020-10-05 07:24:15 -0500

    Updates

---
+++ precache-manifest.48df9295df7eb8aec1a94f19c4a08852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0341341b0365df54546bf70b2f1aaed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "454a8c952308b6316ad6",
+    "url": "/static/css/main.d1481f7c.chunk.css"
+  },
+  {
+    "revision": "2e5408cc347f54f7b74b",
+    "url": "/static/js/2.fd10faaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd10faaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454a8c952308b6316ad6",
+    "url": "/static/js/main.5bbd0cad.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
